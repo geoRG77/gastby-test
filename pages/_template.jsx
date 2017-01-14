92a9602771d3c8f97993b7b73d613f16848e33b9
Jiří Orság <georg77@gmail.com>
2017-01-14 22:21:58 +0100

    Project init

---
+++ pages/_template.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { Container } from 'react-responsive-grid'
+import { Link } from 'react-router'
+import { prefixLink } from 'gatsby-helpers'
+import Headroom from 'react-headroom'
+import '../css/markdown-styles'
+import anime from 'animejs'
+
+import { rhythm } from '../utils/typography'
+
+module.exports = React.createClass({
+  propTypes () {
+    return {
+      children: React.PropTypes.any,
+    }
+  },
+  componentDidMount () {
+    anime({
+      targets: 'div',
+      translateX: '3rem',
+      loop: true
+    });
+  },
+  render () {
+    return (
+      <div>
+        <Headroom
+          wrapperStyle={{
+            marginBottom: rhythm(1),
+          }}
+          style={{
+            background: 'lightgray',
+          }}
+        >
+          <Container
+            style={{
+              maxWidth: 960,
+              paddingTop: 0,
+              padding: `${rhythm(1)} ${rhythm(3/4)}`,
+            }}
+          >
+            <Link
+              to={prefixLink('/')}
+              style={{
+                color: 'black',
+                textDecoration: 'none',
+              }}
+            >
+              Gatsby!!!
+            </Link>
+          </Container>
+        </Headroom>
+        <Container
+          style={{
+            maxWidth: 960,
+            padding: `${rhythm(1)} ${rhythm(3/4)}`,
+            paddingTop: 0,
+          }}
+        >
+          {this.props.children}
+        </Container>
+      </div>
+    )
+  },
+})
